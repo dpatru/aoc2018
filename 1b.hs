a290504a2ad33937158a695d4784c47e0b07c5da
Daniel Patru <dpatru@gmail.com>
2018-12-21 08:12:56 -0500

    1b

---
+++ 1b.hs
@@ -0,0 +1,31 @@
+
+-- main = do
+--   ls <- getContents
+--   putStrLn $ show $ sum [read l :: Int | l <- lines ls]
+
+-- main = do
+--   interact (show . sum . map (read :: String -> Int) . lines)
+--   putStrLn ""
+
+-- main = do
+--   interact (show . sum . map (read) . words)
+--   putStrLn ""
+
+-- import Text.Regex.PCRE.String
+import Data.Set (Set, singleton, member, insert)
+
+stripPlus ('+':rest) = rest
+stripPlus xs = xs
+
+extendList ls = ls ++ extendList ls
+
+process s f (c:cs) =
+  if (f+c) `member` s
+  then f+c
+  else process (insert (f+c) s) (f+c) cs
+
+main = do
+  interact (show . process (singleton 0) 0 . extendList .
+            map (read . stripPlus) . lines)
+  putStrLn ""
+  
