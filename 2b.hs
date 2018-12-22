0c7b1bf88b18bb6bd4bd36666f5a417ae80cb72e
Daniel Patru <dpatru@gmail.com>
2018-12-21 22:31:34 -0500

    3a and b

--- 2b.hs
+++ 2b.hs
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import Data.Set (Set, member, insert, empty)
-
-data Solution = NotFound
-              | Found String String --prefix and suffix
-              | Looking Set String String [String] --prefix, suffix, other words
-
-search :: Solution -> Solution
-search Looking s pre "" 
-check :: Set -> [String] -> Maybe (String, String)
-check s [] = Nothing
-check s (w:ws) = maybe checkWord
-  sort (x:xs) = sort [y | y <- xs, y < x] ++ [x] ++ sort [y | y <- xs, y >= x]
-sort [] = []
-
-checksum (x,y) = x * y
-
-
-count (a, b, n, x) (y:ys)
-  | x==y = count (a, b, n+1,x) ys
-  | n == 2 && b > 0 = (1,1)
-  | n == 2 = count (1, b, 1, y) ys
-  | n == 3 && a > 0 = (1,1)
-  | n == 3 = count (a, 1, 1, y) ys
-  | otherwise = count (a, b, 1, y) ys
-count (a,b,n,x) [] | n == 2 = (1,b)
-                   | n == 3 = (a,1)
-                   | otherwise = (a,b)
-
-
-process (x,y) ys = (x+a, y+b)
-  where (a,b) = count (0,0,0,'a') ys
-
-main = do
-  interact (show . checksum . foldl process (0,0) . map sort . lines)
-  putStrLn ""
-
--- main = do
---   interact (unlines . map sort . lines)
---   putStrLn ""
-=======
 import Data.Set (Set, insert, empty, member)
 
 type MySet = Set (String,String)
@@ -59,4 +18,3 @@ check (Check (x:xs) ys set ws)
 main = do
   interact (show . (check . Begin empty) . lines)
   putStrLn ""
->>>>>>> a1c25a55ac8252ebe52c0139c6f90cbf7e1c8928
--- 3b.hs
+++ 3b.hs
@@ -1,4 +1,4 @@
-import Data.Map.Strict (Map, insertLookupWithKey, empty)
+import Data.Map.Strict as M (Map, insertLookupWithKey, insertWith, empty, foldl', filter)
 
 data Claim = Claim Int Int Int Int Int -- Id, x, y, w, 
   deriving (Show, Read)
@@ -21,11 +21,15 @@ trimHash s = s
 parseClaim line = Claim i x y w h
   where [i,x,y,w,h] = parseNumbers line
 
-squares (Claim id x y w h) = [(x+i,y+j) | i<-[0..w-1], j<-[0..h-1]]
-countOverlaps claims = c
-  where (c, s) = foldl f (0, empty) [s | c<-claims,s<-squares c]
-        f (c, m) sq = (maybe c (\k->if k==1 then c+1 else c) v, m')
-          where (v, m') = insertLookupWithKey (\k n o->n+o) sq 1 m
+squares (Claim n x y w h) = [(n, (x+i,y+j)) | i<-[0..w-1], j<-[0..h-1]]
+
+findSpace sqMap = M.filter (== 1) idMap
+  where idMap = M.foldl' (\m (ns,c)->foldl (\m n-> insertWith max n c m) m ns) empty sqMap
+        
+countOverlaps claims = (c, findSpace m)
+  where (c, m) = foldl f (0, empty) [s | c<-claims,s<-squares c]
+        f (c, m) (n, sq) = (maybe c (\(n,k)->if k==1 then c+1 else c) v, m')
+          where (v, m') = insertLookupWithKey (\k (n2,c2) (n1,c1) ->(n2++n1, c1+c2)) sq ([n],1) m
 
 main = do
   interact (show . countOverlaps . map parseClaim . lines)
